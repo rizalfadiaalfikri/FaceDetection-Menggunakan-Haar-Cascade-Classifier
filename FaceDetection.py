e943b55f4510b4f1094fd7cae89986f868de7016
Rizal Fadia Al Fikri <75767390+rizalfadiaalfikri@users.noreply.github.com>
2021-11-14 11:21:37 +0700

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,5 +1,6 @@
 import cv2
 
+# Create Bounding Box
 def draw_ped(img,label,x0,y0,xt,yt,color=(255,0,255),text_color=(255,255,255)) :
     (w,h), baseline = cv2.getTextSize(label,cv2.FONT_HERSHEY_COMPLEX,0.5,1)
 
@@ -26,22 +27,38 @@ def draw_ped(img,label,x0,y0,xt,yt,color=(255,0,255),text_color=(255,255,255)) :
                 )                       
     return img
 
+# Face Detection Using Image
+# img = cv2.imread("img/family.jpg")
+# face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
-face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
-cap = cv2.VideoCapture(0)
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(gray,1.3,5)
+
+# for (x,y,w,h) in faces:
+#     img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+    
+# cv2.imshow("Frame",img)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# Face Detection Using Webcam
+face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml") # Open Files CaCascadeClassifier
+cap = cv2.VideoCapture(0) # Open Video Stream
 
 while cap.isOpened():
     ret,frame = cap.read()
     
     if ret :
         
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #Convert To Gray Scale
         faces = face_cascade.detectMultiScale(gray,1.3,5)
         
         for (x,y,w,h) in faces :
             frame = draw_ped(frame,"Faces",x,y,x+w,y+h)
         
-        cv2.imshow("Frame",frame)
+        cv2.imshow("Frame",frame) # Show Image
         
         if cv2.waitKey(1) == ord('q') :
             break
